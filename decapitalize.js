7984fb271c3cd59dfeee525d316de79dd613fa8d
Afrina Tithy <afrina.tithy@gmail.com>
2025-05-01 22:36:50 +0600

    decapitalize.js

---
+++ decapitalize.js
@@ -0,0 +1,10 @@
+let str = "Hello World";
+
+let firstLetter = str.charAt(0).toLowerCase();
+
+let restOfString = str.slice(1);
+
+let result = firstLetter + restOfString;
+
+console.log(result);
+s
