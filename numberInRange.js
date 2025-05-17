71487e7bd484943d1412be8704eeff4d40061347
Afrina Tithy <afrina.tithy@gmail.com>
2025-05-17 22:35:35 +0600

    numberInRange.js

---
+++ numberInRange.js
@@ -0,0 +1,9 @@
+let num = 25;
+let start = 10;
+let end = 30;
+
+if (num >= start && num <= end) {
+    console.log(num + " is within the range " + start + " to " + end);
+} else {
+    console.log(num + " is NOT within the range " + start + " to " + end);
+}
